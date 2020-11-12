2b9f4cb69d67505d49b91b9e854dcefe0e38040f
Maxim Alpatski <alpatskij.mi@edu.spbstu.ru>
2020-11-12 22:35:58 +0300

    jar

---
+++ src/main/kotlin/view/Launcher.kt
@@ -0,0 +1,5 @@
+package view
+
+fun main(args: Array<String>) {
+    launchApp(args)
+}
--- src/main/kotlin/view/MyApp.kt
+++ src/main/kotlin/view/MyApp.kt
@@ -1,5 +1,11 @@
+package view
+
 import tornadofx.*
 import view.MainMenu
 import view.Styles
+
+fun main(args: Array<String>) {
+    launch<MyApp>(args)
+}
 
 class MyApp: App(MainMenu::class, Styles:: class)
